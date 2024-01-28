fad0b91eae7e7cf9b53b170b748a768270f33469
Kevin <kevin.illanas@gmail.com>
2024-01-28 20:58:33 +0000

    CHANGE: Removed function that was not used

--- src/stores/mainStore.ts
+++ src/stores/mainStore.ts
@@ -4,16 +4,7 @@ import { reactive } from "vue"
 export const useMainStore = defineStore("mainStore", () => {
     const query: any = reactive({})
 
-    function addQuery(newQuery: any) {
-        Object.entries(newQuery).forEach(([key, value]) => {
-            if (!query.hasOwnProperty(key) || query[key] !== value) {
-                query[key] = value
-            }
-        })
-    }
-
     return {
         query,
-        addQuery,
     }
 })
